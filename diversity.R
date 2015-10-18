b53d0b9173a1d430774fc22692dbc83c29c95aac
Tian-hao <zth1992@gmail.com>
2015-10-18 12:29:05 -0700

    and child

--- diversity.R
+++ diversity.R
@@ -7,5 +7,10 @@ for (filename in filenames){
   tmp <- read.table(tmpname,header=T)
   ddd <- diversity(tmp[,2])
   cat(filename,'\t',ddd,'\n',sep='')
+}    
+for (filename in filenames){
+  tmpname <- paste('../data/',filename,sep='')
+  tmp <- read.table(tmpname,header=T)
+  ddd <- diversity(tmp[,3])
+  cat(filename,'\t',ddd,'\n',sep='')
 }
-    
