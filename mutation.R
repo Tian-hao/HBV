008a25cc1b0b9596debf390fd5acdba07df53059
Tian-hao <zth1992@gmail.com>
2015-10-20 13:57:01 -0700

    add numbers

--- mutation.R
+++ mutation.R
@@ -6,22 +6,41 @@ for (filename in filenames){
   tmp <- read.table(tmpname,header=T)
   outname <- paste('../',filename,'.div',sep='')
   sink(outname)
-  mut <- matrix(,nrow=4,ncol=1254)
+  mutp <- matrix(,nrow=4,ncol=1254)
   for (j in 1:4){
     for (i in 1:1254){
-      mut[j,i] <- 0
+      mutp[j,i] <- 0
+    }
+  }
+  mutc <- matrix(,nrow=4,ncol=1254)
+  for (j in 1:4){
+    for (i in 1:1254){
+      mutc[j,i] <- 0
     }
   }
   for (seq in tmp[,1]){
     for (i in 1:1254){
-      if (substr(seq,i,i)=='A') mut[1,i] <- mut[1,i]+1
-      if (substr(seq,i,i)=='C') mut[2,i] <- mut[2,i]+1
-      if (substr(seq,i,i)=='T') mut[3,i] <- mut[3,i]+1
-      if (substr(seq,i,i)=='G') mut[4,i] <- mut[4,i]+1
+      if (substr(seq,i,i)=='A') {
+        mutp[1,i] <- mutp[1,i]+tmp[tmp[,1]==seq,2]
+	mutc[1,i] <- mutc[1,i]+tmp[tmp[,1]==seq,3]
+      }
+      if (substr(seq,i,i)=='C') {
+        mutp[2,i] <- mutp[2,i]+tmp[tmp[,1]==seq,2]
+	mutc[2,i] <- mutc[2,i]+tmp[tmp[,1]==seq,3]
+      }
+      if (substr(seq,i,i)=='T') {
+        mutp[3,i] <- mutp[3,i]+tmp[tmp[,1]==seq,2]
+	mutc[3,i] <- mutc[3,i]+tmp[tmp[,1]==seq,3]
+      }
+      if (substr(seq,i,i)=='G') {
+        mutp[4,i] <- mutp[4,i]+tmp[tmp[,1]==seq,2]
+	mutc[4,i] <- mutc[4,i]+tmp[tmp[,1]==seq,3]
+      }
     }
   }
   for (i in 1:1254){
-    ddd <- diversity(mut[,i])
-    cat(i,ddd,'\n')
+    ddd <- diversity(mutp[,i])
+    ddc <- diversity(mutc[,i])
+    cat(i,ddd,ddc,'\n')
   }
 }
